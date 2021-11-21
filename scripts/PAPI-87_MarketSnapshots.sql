7db71203381f6aa88ef194c89423cdbf3f4120a9
Tyler Pena <tylerpena@me.com>
2021-11-21 10:41:28 -0800

    Ran and tested script to refactor snapshot JSON details with OHLC

--- scripts/PAPI-87_MarketSnapshots.sql
+++ scripts/PAPI-87_MarketSnapshots.sql
@@ -2,16 +2,18 @@ USE platform;
 
 DELIMITER //
 
-CREATE PROCEDURE BackFillPoolSnapshotOHLC ()
+DROP PROCEDURE IF EXISTS BackFillMarketSnapshotOHLC; //
+
+CREATE PROCEDURE BackFillMarketSnapshotOHLC()
 BEGIN
     DECLARE finished INTEGER DEFAULT 0;
-    DECLARE Id bigint unsigned;
-    DECLARE Details JSON;
+    DECLARE SnapshotId bigint unsigned;
+    DECLARE SnapshotDetails JSON;
 
     -- declare cursor
     DEClARE snapshotCursor
         CURSOR FOR
-            SELECT * FROM market_snapshot WHERE JSON_EXTRACT(Details, '$.liquidityUsd.close') IS NULL;
+            SELECT Id, Details FROM market_snapshot WHERE JSON_EXTRACT(Details, '$.liquidityUsd.close') IS NULL;
 
     -- declare NOT FOUND handler
     DECLARE CONTINUE HANDLER
@@ -20,48 +22,50 @@ BEGIN
     OPEN snapshotCursor;
 
     getSnapshot: LOOP
-        FETCH snapshotCursor INTO Id, Details;
+        FETCH snapshotCursor INTO SnapshotId, SnapshotDetails;
 
         IF finished = 1 THEN
             LEAVE getSnapshot;
         END IF;
 
         -- Build new JSON and update record by Id with new JSON
-        SELECT JSON_MERGE_PRESERVE(
-            JSON_OBJECT('volume', JSON_EXTRACT(@Details, '$.volume')),
+        UPDATE market_snapshot SET Details = (SELECT JSON_MERGE_PRESERVE(
+            JSON_OBJECT('volume', JSON_EXTRACT(SnapshotDetails, '$.volume')),
             JSON_OBJECT('rewards', JSON_MERGE_PRESERVE(
-                                    JSON_OBJECT('marketUsd', JSON_EXTRACT(@Details, '$.rewards.marketUsd')),
-                                    JSON_OBJECT('providerUsd', JSON_EXTRACT(@Details, '$.rewards.providerUsd'))
+                                    JSON_OBJECT('marketUsd', JSON_EXTRACT(SnapshotDetails, '$.rewards.marketUsd')),
+                                    JSON_OBJECT('providerUsd', JSON_EXTRACT(SnapshotDetails, '$.rewards.providerUsd'))
                                   )
             ),
             JSON_OBJECT('staking', JSON_MERGE_PRESERVE(
                 JSON_OBJECT('usd', JSON_MERGE_PRESERVE(
-                                            JSON_OBJECT('low', JSON_EXTRACT(@Details, '$.staking.usd')),
-                                            JSON_OBJECT('high', JSON_EXTRACT(@Details, '$.staking.usd')),
-                                            JSON_OBJECT('open', JSON_EXTRACT(@Details, '$.staking.usd')),
-                                            JSON_OBJECT('close', JSON_EXTRACT(@Details, '$.staking.usd'))
+                                            JSON_OBJECT('low', JSON_EXTRACT(SnapshotDetails, '$.staking.usd')),
+                                            JSON_OBJECT('high', JSON_EXTRACT(SnapshotDetails, '$.staking.usd')),
+                                            JSON_OBJECT('open', JSON_EXTRACT(SnapshotDetails, '$.staking.usd')),
+                                            JSON_OBJECT('close', JSON_EXTRACT(SnapshotDetails, '$.staking.usd'))
                                         )
                 ),
                 JSON_OBJECT('weight', JSON_MERGE_PRESERVE(
-                                            JSON_OBJECT('low', JSON_EXTRACT(@Details, '$.staking.weight')),
-                                            JSON_OBJECT('high', JSON_EXTRACT(@Details, '$.staking.weight')),
-                                            JSON_OBJECT('open', JSON_EXTRACT(@Details, '$.staking.weight')),
-                                            JSON_OBJECT('close', JSON_EXTRACT(@Details, '$.staking.weight'))
+                                            JSON_OBJECT('low', JSON_EXTRACT(SnapshotDetails, '$.staking.weight')),
+                                            JSON_OBJECT('high', JSON_EXTRACT(SnapshotDetails, '$.staking.weight')),
+                                            JSON_OBJECT('open', JSON_EXTRACT(SnapshotDetails, '$.staking.weight')),
+                                            JSON_OBJECT('close', JSON_EXTRACT(SnapshotDetails, '$.staking.weight'))
                                         )
                 )
             )),
             JSON_OBJECT('liquidityUsd', JSON_MERGE_PRESERVE(
-                                            JSON_OBJECT('low', JSON_EXTRACT(@Details, '$.liquidity')),
-                                            JSON_OBJECT('high', JSON_EXTRACT(@Details, '$.liquidity')),
-                                            JSON_OBJECT('open', JSON_EXTRACT(@Details, '$.liquidity')),
-                                            JSON_OBJECT('close', JSON_EXTRACT(@Details, '$.liquidity'))
+                                            JSON_OBJECT('low', JSON_EXTRACT(SnapshotDetails, '$.liquidity')),
+                                            JSON_OBJECT('high', JSON_EXTRACT(SnapshotDetails, '$.liquidity')),
+                                            JSON_OBJECT('open', JSON_EXTRACT(SnapshotDetails, '$.liquidity')),
+                                            JSON_OBJECT('close', JSON_EXTRACT(SnapshotDetails, '$.liquidity'))
                                         ))
-        ) INTO @UpdatedDetails;
-
-        UPDATE market_snapshot SET Details = @UpdatedDetails WHERE Id = @Id;
+        )) WHERE Id = SnapshotId;
     END LOOP getSnapshot;
     CLOSE snapshotCursor;
 
 END //
+
+CALL BackFillMarketSnapshotOHLC(); //
+
+DROP PROCEDURE BackFillMarketSnapshotOHLC; //
 
 DELIMITER ;
