c108cdd8f284feeab7de88801a647f1db32cd1d6
drmathias <adam@opdex.com>
2022-02-14 14:38:53 +0000

    [PAPI-375] Auth success table

---
+++ scripts/PAPI-375_AuthChallenges.sql
@@ -0,0 +1,26 @@
+DELIMITER //
+
+CREATE PROCEDURE AuthChallenges ()
+ BEGIN
+        CREATE TABLE IF NOT EXISTS auth_success(
+            ConnectionId       VARCHAR(255) NOT NULL,
+            Signer             VARCHAR(50) NOT NULL,
+            Expiry             DATETIME NOT NULL,
+            PRIMARY KEY (ConnectionId)
+        ) ENGINE = INNODB;
+ END;
+//
+
+CALL AuthChallenges();
+//
+
+DROP PROCEDURE AuthChallenges;
+//
+
+CREATE EVENT IF NOT EXISTS remove_expired_auth_success_event
+ON SCHEDULE EVERY 5 MINUTE
+DO
+DELETE FROM auth_success WHERE auth_success.Expiry < UTC_TIMESTAMP();
+//
+
+DELIMITER ;
